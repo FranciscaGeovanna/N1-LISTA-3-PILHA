583d7129099f299e288939ef22a3f10831591dde
FranciscaGeovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2023-05-14 15:15:12 -0300

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,56 @@
+print("Transforma binário em décimal")
+
+class Item:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Pilha:
+    def __init__(self):
+        self._top = None
+        self.size = 0
+   
+    def __len__(self):
+        return self.size
+    
+    def push(self, value):
+        novoItem = Item(value)
+        novoItem.next = self._top
+        self._top = novoItem
+        self.size += 1
+   
+    def pop(self):
+        if self.size == 0:
+            raise Exception("\nA pilha está vazia!")
+        valor = self._top.value
+        self._top = self._top.next
+        self.size -= 1
+        return valor
+
+    def top(self):
+        if self.is_empty():
+            raise IndexError("A pilha está vazia")
+        return self._top.value
+    
+    def is_empty(self):
+        return self._top is None
+
+def transforma(binario):
+    pilha = Pilha()
+    
+    for p in binario:
+        pilha.push(int(p))
+   
+    potencia = 0
+    somaPotencias = 0
+     
+    while not pilha.is_empty():
+        somaPotencias += pilha.pop() * (2 ** potencia)
+        potencia += 1
+    
+    return somaPotencias
+    
+numero = input("\nDigite um número binário: ")
+
+decimal = transforma(numero)
+print(f"\nO número {numero} em decimal é: ", decimal)
